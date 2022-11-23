ca0da17120cdcba0988fd309409a91560878a826
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-11-22 23:24:14 -0500

    Updated Aboutme

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../../images/Logo2.png';
+import Ivr from '../../images/IVR.png';
 
 export default function About() {
   return (
     <div className="abouttext">
       <h1>Welcome to Iva Studios</h1>
+      <img id="ivr" src={Ivr}></img>
       <p>
         Founded in 2022. Fueled by my love and passion for finding out how
         things work and how to fix it.
       </p>
-
       <img id="logo" src={logo}></img>
     </div>
   );
